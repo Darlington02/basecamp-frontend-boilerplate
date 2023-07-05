a304e99193acaf3264d1ffe42305f3d4af34a589
Darlington02 <darlingtonnnam@gmail.com>
2023-07-05 23:48:28 +0100

    completed basic demo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+import logo from './images/argent.png';
 import './App.css';
+import { connect, disconnect } from '@argent/get-starknet';
+import { useEffect, useState } from 'react';
 
 function App() {
+ const [connection, setConnection] = useState('')
+ const [provider, setProvider] = useState('')
+ const [address, setAddress] = useState('')
+
+ useEffect(() => {
+  const connectToStarknet = async() => {
+    const connection = await connect({modalMode: "neverAsk", webWalletUrl: "https://web.argent.xyz"})
+    
+    if(connection && connection.isConnected) {
+      setConnection(connection)
+      setProvider(connection.account)
+      setAddress(connection.selectedAddress)
+    }
+  }
+  connectToStarknet()
+ }, [])
+
+ const connectWallet = async() => {
+  const connection = await connect({webWalletUrl: "https://web.argent.xyz"});
+
+  if(connection && connection.isConnected) {
+    setConnection(connection)
+    setProvider(connection.account)
+    setAddress(connection.selectedAddress)
+  }
+ }
+
+ const disconnectWallet = async() => {
+  await disconnect()
+  setConnection(undefined)
+  setProvider(undefined)
+  setAddress('')
+ }
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='title'>
+          Address: {address}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          !connection ? <button className='connectbtn' onClick={connectWallet}>Connect</button>
+          : <button className='connectbtn' onClick={disconnectWallet}>Disconnect</button>
+        }
+        
       </header>
     </div>
   );
